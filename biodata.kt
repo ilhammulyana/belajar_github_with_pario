9d1b67a2dc64eef817ea65b4d4fa155226da9908
ilhammulyana <145539367+ilhammulyana@users.noreply.github.com>
2023-09-20 15:10:18 +0700

    Create biodata.kt

---
+++ biodata.kt
@@ -0,0 +1,11 @@
+fun main()
+{
+  var nama = "Rio Adrian M G"
+  var kelas = "XI RPL 2"
+  var hobi = "playing bola"
+
+  println("Nama: $nama")
+  println("Kelas: $kelas")
+  println("Hobi: $hobi")
+  
+}
